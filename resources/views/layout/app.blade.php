e67c92e8ec02cebdd52077aee56a4da69e975a6e
Jacob okoro <jaycobokoro@gmail.com>
2018-11-06 20:00:44 +0100

    ran npm install created sidebar installed nodemon

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,10 +3,20 @@
 <head>
 	<meta charset="utf-8">
 	<title>Acme</title>
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-	@yield('content')
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-lg-8">
+				@yield('content')
+			</div>
+			<div class="col-md-4 col-lg-4">
+				@include('inc.sidebar')
+			</div>
+		</div>
+	</div>
 
-	@include('inc.sidebar')
 </body>
 </html>
