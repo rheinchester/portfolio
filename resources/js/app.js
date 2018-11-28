35cecfa141b933051949f04192673fa35102f394
Jacob okoro <jaycobokoro@gmail.com>
2018-11-28 05:23:02 +0100

    1) imported now ui kit 2) successfully cmpiled the sass and the js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,8 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
-
+import './bootstrap';
+import './now-ui-kit'
 window.Vue = require('vue');
 
 /**
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .copy('node_modules/now-ui-kit/assets/js/now-ui-kit.js','resources/js');
+ 
