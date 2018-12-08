91edd52dd8a1bc3b870cfda618980db99e944144
Jacob okoro <jaycobokoro@gmail.com>
2018-12-08 22:58:45 +0100

    linked gallery to post

--- app/Gallery.php
+++ app/Gallery.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Gallery extends Model
 {
-    //
+    protected $table = 'galleries';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+
+    public function user() {
+        //Relates post to user
+        return $this->belongsTo('App\User');
+    } 
 }
