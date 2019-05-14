3c4f704b58fa6d6c88ac159719cbbe4bfb8ea889
Jacob okoro <jaycobokoro@gmail.com>
2019-05-14 17:00:46 +0100

    removed vendor from gitignore

---
+++ vendor/get-stream/stream/lib/GetStream/Stream/Client.php
@@ -0,0 +1,324 @@
+<?php
+namespace GetStream\Stream;
+
+use DateTime;
+use Exception;
+
+const VERSION = '2.6.0';
+
+class Client
+{
+    const API_ENDPOINT = 'stream-io-api.com/api';
+
+    /**
+     * @var string
+     */
+    protected $api_key;
+
+    /**
+     * @var string
+     */
+    protected $api_secret;
+
+    /**
+     * @var string
+     */
+    protected $location;
+
+    /**
+     * @var string
+     */
+    protected $protocol;
+
+    /**
+     * @var Signer
+     */
+    public $signer;
+
+    /**
+     * @var string
+     */
+    public $api_version;
+
+    /**
+     * @var float
+     */
+    public $timeout;
+
+    /**
+     * @param string $api_key
+     * @param string $api_secret
+     * @param string $api_version
+     * @param string $location
+     * @param float $timeout
+     */
+    public function __construct($api_key, $api_secret, $api_version='v1.0', $location='', $timeout=3.0)
+    {
+        $this->api_key = $api_key;
+        $this->api_secret = $api_secret;
+        $this->signer = new Signer($api_key, $api_secret);
+        $this->api_version = $api_version;
+        $this->timeout = $timeout;
+        $this->location = $location;
+        $this->protocol = 'https';
+    }
+
+    /**
+     * @param  string|null $url
+     * @return Client
+     * @throws Exception
+     */
+    public static function herokuConnect($url = null)
+    {
+        if ($url === null) {
+            $url = getenv('STREAM_URL');
+        }
+
+        $parsed_url = parse_url($url);
+        $api_key = $parsed_url['user'];
+        $api_secret = $parsed_url['pass'];
+
+        if ($api_key == '' || $api_secret == '') {
+            throw new Exception('url malformed');
+        }
+        $client = new static($api_key, $api_secret);
+        $location = explode('stream-io-api.com', $parsed_url['host'])[0];
+        $location = str_replace('.', '', $location);
+        $client->setLocation($location);
+        return $client;
+    }
+
+    /**
+     * @param  string $protocol
+     */
+    public function setProtocol($protocol)
+    {
+        $this->protocol = $protocol;
+    }
+
+    /**
+     * @param  string $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * @param  string $user_id
+     * @param  array $extra_data
+     * @return string
+     */
+    public function createUserSessionToken($user_id, array $extra_data=null)
+    {
+        if(is_null($extra_data)){
+            $extra_data = array();
+        }
+        return $this->createUserToken($user_id, $extra_data);
+    }
+
+    /**
+     * @param  string $user_id
+     * @param  array $extra_data
+     * @return string
+     */
+    public function createUserToken($user_id, array $extra_data=null)
+    {
+        if(is_null($extra_data)){
+            $extra_data = array();
+        }
+        return $this->signer->jwtUserSessionToken($user_id, $extra_data);
+    }
+
+    /**
+     * @param  BaseFeed $feed
+     * @param  string $resource
+     * @param  string $action
+     * @return string
+     */
+    public function createFeedJWTToken($feed, $resource, $action)
+    {
+        $feedId = "{$feed->getSlug()}{$feed->getUserId()}";
+        return $this->signer->jwtScopeToken($feedId, $resource, $action);
+    }
+
+    /**
+     * @param  string $feed_slug
+     * @param  string $user_id
+     * @param  string|null $token
+     * @return Feed
+     */
+    public function feed($feed_slug, $user_id, $token = null)
+    {
+        if (null === $token) {
+            $token = $this->signer->signature($feed_slug . $user_id);
+        }
+        return new Feed($this, $feed_slug, $user_id, $this->api_key, $token);
+    }
+
+    /**
+     * @return Batcher
+     */
+    public function batcher()
+    {
+        return new Batcher($this, $this->signer, $this->api_key);
+    }
+
+    /**
+     * @return Personalization
+     */
+    public function personalization()
+    {
+        return new Personalization($this, $this->api_key, $this->api_secret);
+    }
+
+    /**
+     * @return Collections
+     */
+    public function collections()
+    {
+        return new Collections($this, $this->api_key, $this->api_secret);
+    }
+
+    /**
+     * @return Reactions
+     */
+    public function reactions()
+    {
+        return new Reactions($this, $this->api_key, $this->api_secret);
+    }
+
+    /**
+     * @return Users
+     */
+    public function users()
+    {
+        return new Users($this, $this->api_key, $this->api_secret);
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseUrl()
+    {
+        $baseUrl = getenv('STREAM_BASE_URL');
+        if (!$baseUrl) {
+            $api_endpoint = static::API_ENDPOINT;
+            $localPort = getenv('STREAM_LOCAL_API_PORT');
+            if ($localPort) {
+                $baseUrl = "http://localhost:$localPort/api";
+            } else {
+                if ($this->location) {
+                    $subdomain = "{$this->location}-api";
+                } else {
+                    $subdomain = 'api';
+                }
+                $baseUrl = "{$this->protocol}://{$subdomain}." . $api_endpoint;
+            }
+        }
+        return $baseUrl;
+    }
+
+    /**
+     * @param  string $uri
+     * @return string
+     */
+    public function buildRequestUrl($uri)
+    {
+        $baseUrl = $this->getBaseUrl();
+        return "{$baseUrl}/{$this->api_version}/{$uri}";
+    }
+
+    public function getActivities($ids=null, $foreign_id_times=null)
+    {
+        if($ids!==null){
+            $query_params = ["ids" => join(',', $ids)];
+        } else {
+            $fids = [];
+            $times = [];
+            foreach($foreign_id_times as $fit){
+                $fids[] = $fit[0];
+                try {
+                    $times[] = $fit[1]->format(DateTime::ISO8601);
+                } catch(Exception $e) {
+                    // assume it's in the right format already
+                    $times[] = $fit[1];
+                }
+            }
+            $query_params = [
+                "foreign_ids" => join(',', $fids),
+                "timestamps" => join(',', $times)
+            ];
+
+        }
+        $token = $this->signer->jwtScopeToken('*', 'activities', '*');
+        $activities = new Activities($this, $this->api_key, $token);
+        return $activities->_getActivities($query_params);
+    }
+
+    public function batchPartialActivityUpdate($data)
+    {
+        if(count($data) > 100){
+            throw new Exception("Max 100 activities allowed in batch update");
+        }
+        $token = $this->signer->jwtScopeToken('*', 'activities', '*');
+        $activityUpdateOp = new ActivityUpdateOperation($this, $this->api_key, $token);
+        return $activityUpdateOp->partiallyUpdateActivity(["changes" => $data]);
+    }
+
+    public function doPartialActivityUpdate($id=null, $foreign_id=null, $time=null, $set=null, $unset=null)
+    {
+        $token = $this->signer->jwtScopeToken('*', 'activities', '*');
+        if($id === null && ($foreign_id === null || $time === null)){
+            throw new Exception(
+                "The id or foreign_id+time parameters must be provided and not be None"
+            );
+        }
+        if($id !== null && ($foreign_id !== null || $time !== null)){
+            throw new Exception(
+                "Only one of the id or the foreign_id+time parameters can be provided"
+            );
+        }
+
+        $data = ["set" => $set, "unset" => $unset];
+
+        if($id !== null){
+            $data["id"] = $id;
+        } else {
+            $data["foreign_id"] = $foreign_id;
+            $data["time"] = $time;
+        }
+        $activityUpdateOp = new ActivityUpdateOperation($this, $this->api_key, $token);
+        return $activityUpdateOp->partiallyUpdateActivity($data);
+    }
+
+    public function updateActivities($activities)
+    {
+        if (empty($activities)) {
+            return;
+        }
+        $token = $this->signer->jwtScopeToken('*', 'activities', '*');
+        $activityUpdateOp = new ActivityUpdateOperation($this, $this->api_key, $token);
+        return $activityUpdateOp->updateActivities($activities);
+    }
+
+    public function updateActivity($activity)
+    {
+        return $this->updateActivities([$activity]);
+    }
+
+    /**
+     * Creates a redirect url for tracking the given events in the context of
+     * getstream.io/personalization
+     * @param  string $targetUrl
+     * @param  array $events
+     * @return string
+     */
+    public function createRedirectUrl($targetUrl, $events)
+    {
+        $token = $this->signer->jwtScopeToken('*', 'redirect_and_track', '*');
+        $analytics = new Analytics($this, $this->api_key, $token);
+        return $analytics->createRedirectUrl($targetUrl, $events);
+    }
+
+}
---
+++ vendor/get-stream/stream/lib/GetStream/Stream/Collections.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace GetStream\Stream;
+
+use Firebase\JWT\JWT;
+use GetStream\Stream\Client as StreamClient;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\HandlerStack;
+use Psr\Http\Message\RequestInterface;
+
+class Collections
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    /**
+     * @var string
+     */
+    private $apiSecret;
+
+    /**
+     * @param \GetStream\Stream\Client $streamClient
+     * @param string $apiKey
+     * @param string $apiSecret
+     */
+    public function __construct(StreamClient $streamClient, $apiKey, $apiSecret)
+    {
+        $this->apiKey = $apiKey;
+        $this->apiSecret = $apiSecret;
+        $this->client = new GuzzleClient([
+            'base_uri' => $streamClient->getBaseUrl().'/'.$streamClient->api_version.'/',
+            'timeout' => $streamClient->timeout,
+            'handler' => $this->handlerStack(),
+        ]);
+    }
+
+    private function doRequest($method, $endpoint, $params=null)
+    {
+        if($params === null){
+            $params = array();
+        }
+        if( $method === 'POST' || $method === 'PUT' ){
+            $params = ['json' => $params];
+        }
+        if( $method === 'GET' && $params !== null ){
+
+            $endpoint .= '?' . http_build_query($params);
+        }
+        try {
+            $response = $this->client->request($method, $endpoint, $params);
+        } catch (ClientException $e) {
+            $response = $e->getResponse();
+            $msg = $response->getBody();
+            $code = $response->getStatusCode();
+            $previous = $e;
+            throw new StreamFeedException($msg, $code, $previous);
+        }
+        return $response;
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array $data
+     * @param string $id (optional)
+     * @param string $user_id (optional)
+     *
+     * @return array
+     */
+    public function add($collectionName, array $data, $id=null, $user_id=null)
+    {
+        $payload = ["id" => $id, "data" => $data, "user_id" => $user_id];
+        $response = $this->doRequest('POST', 'collections/' . $collectionName . '/', $payload);
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string $id
+     *
+     * @return string
+     */
+    public function createReference($collectionName, $id)
+    {
+        return "SO:".$collectionName.":".$id;
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string $id
+     *
+     * @return array
+     */
+    public function delete($collectionName, $id)
+    {
+
+        $response = $this->doRequest('DELETE', 'collections/' . $collectionName . '/' . $id . '/');
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array $ids
+     *
+     * @return array
+     */
+    public function deleteMany($collectionName, array $ids)
+     {
+        $ids = join(',', $ids);
+        $queryParams = ['collection_name' => $collectionName, 'ids' => $ids];
+        $response = $this->client->request('DELETE', 'collections/?'.http_build_query($queryParams));
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
+     }
+
+    /**
+     * @param string $collectionName
+     * @param string $id
+     *
+     * @return array
+     */
+    public function get($collectionName, $id)
+    {
+        $response = $this->doRequest('GET', 'collections/' . $collectionName . '/' . $id . '/');
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array $ids
+     *
+     * @return array
+     */
+    public function select($collectionName, array $ids)
+    {
+        $mappedIds = array_map(function ($id) use ($collectionName) {
+            return sprintf('%s:%s', $collectionName, $id);
+        }, $ids);
+        $params = ['foreign_ids' => join(',', $mappedIds)];
+        $response = $this->doRequest('GET', 'meta/', $params);
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string $id
+     * @param array $data
+     *
+     * @return array
+     */
+    public function update($collectionName, $id, array $data)
+    {
+        $payload = ["data" => $data];
+        $response = $this->doRequest('PUT', 'collections/' . $collectionName . '/' . $id . '/', $payload);
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array $data
+     *
+     * @return array
+     */
+    public function upsert($collectionName, array $data)
+    {
+        $response = $this->doRequest('POST', 'meta/', ['data' => [$collectionName => array($data)]]);
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
+    }
+
+    /**
+     * @return HandlerStack
+     */
+    private function handlerStack()
+    {
+        $token = JWT::encode([
+            'action' => '*',
+            'user_id' => '*',
+            'feed_id' => '*',
+            'resource' => 'collections',
+        ], $this->apiSecret, 'HS256');
+
+        $stack = HandlerStack::create();
+        $stack->push(function (callable $handler) use ($token) {
+            return function (RequestInterface $request, array $options) use ($handler, $token) {
+                // Add authentication headers.
+                $request = $request
+                    ->withAddedHeader('Authorization', $token)
+                    ->withAddedHeader('Stream-Auth-Type', 'jwt')
+                    ->withAddedHeader('Content-Type', 'application/json')
+                    ->withAddedHeader('X-Stream-Client', 'stream-php-client-' . VERSION);
+
+                // Add a api_key query param.
+                $queryParams = \GuzzleHttp\Psr7\parse_query($request->getUri()->getQuery());
+                $query = http_build_query($queryParams + ['api_key' => $this->apiKey]);
+                $request = $request->withUri($request->getUri()->withQuery($query));
+                return $handler($request, $options);
+            };
+        });
+
+        return $stack;
+    }
+}
