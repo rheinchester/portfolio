ed3639a7520a21fbdd6dc76b3d839e0696fff599
Jacob okoro <jaycobokoro@gmail.com>
2019-05-14 17:34:34 +0100

    Altered Home.js and About.js

---
+++ resources/js/components/About.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+
+class About extends PureComponent {
+  render() {
+    return (
+      <div>
+        This is About
+      </div>
+    )
+  }
+}
+export default About
---
+++ resources/js/components/Home.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+
+class Home extends PureComponent {
+  render() {
+    return (
+      <div>
+        This is Home
+      </div>
+    )
+  }
+}
+
+
+export default Home;
