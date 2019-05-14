a1f6265a13ca28ba0bb6870edc6d7021e3c21f12
Jacob okoro <jaycobokoro@gmail.com>
2019-05-14 16:48:01 +0100

    Created Header component. Next commit after this will come from the react branch

---
+++ resources/js/components/About.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+
+class About extends PureComponent {
+    render() {
+        return (
+            <div className="card">
+                I am about
+      </div>
+        )
+    }
+}
+export default About
---
+++ resources/js/components/Home.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+
+class Home extends PureComponent {
+    render() {
+        return (
+        <div className="card">
+            I am Home
+        </div>
+        )
+    }
+}
+
+
+export default Home
