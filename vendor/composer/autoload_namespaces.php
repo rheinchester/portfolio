1d490b76a474e52e115999abd8c12225ec506d7f
Jacob okoro <jaycobokoro@gmail.com>
2019-06-15 14:26:46 +0100

    composer update
    Signed-off-by: Jacob okoro <jaycobokoro@gmail.com>

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,9 +6,9 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'UpdateHelper\\' => array($vendorDir . '/kylekatarnls/update-helper/src'),
     'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Parsedown' => array($vendorDir . '/erusev/parsedown'),
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),
     'GetStream\\Stream' => array($vendorDir . '/get-stream/stream/lib'),
-    'Doctrine\\Common\\Lexer\\' => array($vendorDir . '/doctrine/lexer/lib'),
 );
